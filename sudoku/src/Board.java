55deae308ec1918c5fcc79585af2e270caeac0b9
Aiden Vehemente <avehemente@berkeley.edu>
2021-07-26 08:41:46 -0700

    changed numbering system for position

--- sudoku/src/Board.java
+++ sudoku/src/Board.java
@@ -5,11 +5,13 @@ public class Board {
     private boolean solved;
     private ArrayList<HashSet<Integer>> filled;
     private int[][] positions;
+    private int numsFilled;
 
     /**
      * The Board Constructor
      */
     public Board() {
+        this.numsFilled = 0;
         this.solved = false;
         this.positions = new int[9][9];
         for (int i = 0; i < 9; i++) {
@@ -33,12 +35,12 @@ public class Board {
      * @return True if the number was successfully placed there.
      */
     public boolean placeNumber(int entered, int x, int y) {
-        //place number at position x, y: (0,0) is bottom left
-        //boxes are numbered 0 in the bottomleft, 8 in the top right, increases left to right, bottom to top
+        //place number at position x, y: (0,0) is top left
+        //boxes are numbered 0 in the top left, 8 in the bottom right, increases left to right, top to bottom
         int boxNumber = getBoxNumber(x,y);
 
-        HashSet<Integer> curRow = this.filled.get(9 + x);
-        HashSet<Integer> curCol = this.filled.get(y);
+        HashSet<Integer> curRow = this.filled.get(y);
+        HashSet<Integer> curCol = this.filled.get(9 + x);
         HashSet<Integer> curBox = this.filled.get(18 + boxNumber);
         if (curRow.contains(entered) || curCol.contains(entered) || curBox.contains(entered)) {
             return false;
@@ -66,11 +68,16 @@ public class Board {
         return true;
     }
 
+    public boolean validBoardState() {
+
+        return true;
+    }
     /**
      * Check if the board is in a solved state.
      * @return True if the board is in a solved state (i.e. all rows, cols, and boxes have 1-9)
      */
     public boolean isSolved() {
+        if (this.numsFilled < 81) return false;
         for (HashSet<Integer> cur: this.filled) {
             for (int i = 1; i <= 9; i++) {
                 if (!cur.contains(i)) return false;
@@ -85,32 +92,34 @@ public class Board {
      * @param y The y coordinate.
      * @return The box number corresponding to the given coordinates.
      */
-    public int getBoxNumber(int x, int y) {
-        int boxNumber;
+    public static int getBoxNumber(int x, int y) {
 
         if (y < 3) {
-            if (x < 3) boxNumber = 0;
-            else if (x < 6) boxNumber = 1;
-            else boxNumber = 2;
+            if (x < 3) return 0;
+            else if (x < 6) return 1;
+            else return 2;
         }
         else if (y < 6) {
-            if (x < 3) boxNumber = 3;
-            else if (x < 6) boxNumber = 4;
-            else boxNumber = 5;
+            if (x < 3) return 3;
+            else if (x < 6) return 4;
+            else return 5;
         }
         else {
-            if (x < 3) boxNumber = 6;
-            else if (x < 6) boxNumber = 7;
-            else boxNumber = 8;
+            if (x < 3) return 6;
+            else if (x < 6) return 7;
+            else return 8;
         }
 
-        return boxNumber;
     }
 
     /**
      * Solve this board.
      */
-    public void solve() {
+    public void solve(int x, int y) {
+
+    }
+
+    public void generateBoard() {
 
     }
 }
