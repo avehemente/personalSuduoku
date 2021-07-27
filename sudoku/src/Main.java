963e9f546cd03a3ed6da540037d3aa572a1a33d6
Aiden Vehemente <avehemente@berkeley.edu>
2021-07-26 18:13:36 -0700

    added an example board to generate and test on (will do generation method later)

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -3,9 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Board b = new Board();
-        b.placeNumber(7, 8, 8);
-        b.placeNumber(7, 0,1);
-        b.placeNumber(7, 7, 8);
+        b.generateBoard();
+        b.solve();
         System.out.println(b);
         System.out.println(b.validBoardState());
     }
